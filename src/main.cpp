5105ce845e02282e7023b2ab2d1ec7abc7c9250e
Krupnyk-Ivan <147716414+Krupnyk-Ivan@users.noreply.github.com>
2025-01-29 23:28:38 +0200

    Try implement imgui

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include <SFML/Graphics.hpp>
 #include "GameApp/GameApp.hpp"
+#include "..\build\_deps\imgui-src\imgui.h"
 
 int main()
 {
     auto window = sf::RenderWindow(sf::VideoMode({800u, 600u}), "CMake SFML Project");
-    GameApp* app = new GameApp(&window);
+    GameApp *app = new GameApp(&window);
 
     app->run();
-    
+
     return EXIT_SUCCESS;
 }
