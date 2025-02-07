94fc0984398407f2ce3b25c28938d963bde93985
Krupnyk-Ivan <147716414+Krupnyk-Ivan@users.noreply.github.com>
2025-02-07 19:31:39 +0200

    working imgui sfml

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,45 @@
-#include <SFML/Graphics.hpp>
-#include "GameApp/GameApp.hpp"
-#include "..\build\_deps\imgui-src\imgui.h"
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
 
-int main()
-{
-    auto window = sf::RenderWindow(sf::VideoMode({800u, 600u}), "CMake SFML Project");
-    GameApp *app = new GameApp(&window);
+#include "imgui-SFML.h"
+#include "imgui.h"
 
-    app->run();
+int main() {
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "ImGui + SFML = <3");
+    window.setFramerateLimit(60);
+    ImGui::SFML::Init(window);
 
-    return EXIT_SUCCESS;
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    sf::Clock deltaClock;
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            ImGui::SFML::ProcessEvent(event);
+
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        ImGui::Begin("Hello, world!");
+        ImGui::Button("Look at this pretty button");
+        ImGui::End();
+
+        ImGui::ShowDemoWindow();
+
+        window.clear();
+        window.draw(shape);
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+
+    ImGui::SFML::Shutdown();
+
+    return 0;
 }
